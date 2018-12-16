e926d57c9584c45f70e1c3733392f8a39037b190
ziyanwang <756474646@qq.com>
2018-12-16 13:59:29 +0800

    Use another way to build road in space.

---
+++ Assets/Script/build_road.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class build_road : MonoBehaviour {
+
+    public GameObject cube;
+    public int roadWidth;
+    //private MainLine MainLineCom;
+    private GameObject road;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+        {
+           // road = Instantiate(cube, new Vector3(cube.transform.position.x, cube.transform.position.y - 1, cube.transform.position.z), cube.transform.rotation);
+            //road.transform.localScale = new Vector3(cube.transform.localScale.x + roadWidth, 1f, cube.transform.localScale.z + roadWidth);
+        }
+    }
+}
