e6e890ea9ed194a6bb89229fb1faad75bda5882f
ziyanwang <756474646@qq.com>
2018-12-20 21:46:35 +0800

    Add trigger and script to permit piano key press when cube go.

---
+++ Assets/PianoPlay.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PianoPlay : MonoBehaviour {
+
+    public Animator anim;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            anim.SetBool("Play", true);
+        }
+    }
+}
