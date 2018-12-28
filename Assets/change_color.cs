c7c33dee5fc1b71ab1021ae440ba22e2ccb915bc
ziyanwang <756474646@qq.com>
2018-12-28 19:59:17 +0800

    Add script to change color when trigger and add some new assets.

---
+++ Assets/change_color.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class change_color : MonoBehaviour {
+    public Material material;
+    public Color colorNow;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            material.color = colorNow;
+        }
+    }
+}
