d4b5960846c17cbb504cf4f0fd315164566f5631
ziyanwang <756474646@qq.com>
2018-12-28 23:42:01 +0800

    Add animations. Add script for animation. Add more objects.

---
+++ Assets/getUp.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class getUp : MonoBehaviour {
+    public Animator anim1;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            anim1.SetBool("getUp", true);
+        }
+    }
+}
