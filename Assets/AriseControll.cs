1decbfe9dffec37c16f6334fda00e9ec39db8b28
nancy <nanxinxiao@gmail.com>
2018-12-28 20:11:55 +0800

    make the piano map more difficult

---
+++ Assets/AriseControll.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AriseControll : MonoBehaviour {
+
+    public Animator anim;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            anim.SetBool("Arise", true);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/PianoController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PianoController : MonoBehaviour {
+
+    public Animator anim1;
+    public Animator anim2;
+    public Animator anim3;
+    public Animator anim4;
+    public Animator anim5;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            anim1.SetBool("Arise", true);
+            anim2.SetBool("Arise", true);
+            anim3.SetBool("Arise", true);
+            anim4.SetBool("Arise", true);
+            anim5.SetBool("Arise", true);
+
+        }
+    }
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
