3b46de498e2000d2fb315af267bd9cba086450fd
nancy <nanxinxiao@gmail.com>
2018-12-30 14:46:35 +0800

    add some animator in piano map

---
+++ Assets/PianoColorControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PianoColorControl : MonoBehaviour {
+    
+    public Color colorNow;
+    public GameObject cube;
+    public Animator anim;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player1")
+        {
+            //material.color = colorNow;
+            anim.SetBool("Play", true);
+            cube.GetComponent<MeshRenderer>().material.color = colorNow;
+            
+        }
+    }
+}
