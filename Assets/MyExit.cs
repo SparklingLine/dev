8c8464f9bd3a8a9abfbef3aa59e3344e948d0102
YanYan8150 <45511690+YanYan8150@users.noreply.github.com>
2019-01-02 23:56:36 +0800

    加入菜单

---
+++ Assets/MyExit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyExit : MonoBehaviour {
+
+    // Use this for initialization
+    public void Click()
+    {
+        Application.Quit();
+    }
+}
